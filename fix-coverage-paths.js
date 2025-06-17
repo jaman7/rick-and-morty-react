5428302accfa3b0f6c016fd1131384e995d4d6db
jaman7 <krzysztofbalon123@gmail.com>
2025-06-17 12:00:28 +0200

    fix(coverage): correct relative paths in coverage report 3

--- fix-coverage-paths.js
+++ fix-coverage-paths.js
@@ -1,12 +1,10 @@
 import fs from 'fs';
 
-const path = './coverage/index.html';
+const path = './coverage/html/index.html'; // <- poprawiona ścieżka
 
 try {
   let content = fs.readFileSync(path, 'utf-8');
-
   content = content.replace(/href="(?!http|\.\/)/g, 'href="./').replace(/src="(?!http|\.\/)/g, 'src="./');
-
   fs.writeFileSync(path, content);
   console.log('✅ index.html paths fixed successfully');
 } catch (error) {
