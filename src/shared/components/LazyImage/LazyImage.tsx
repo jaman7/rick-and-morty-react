6017923e94697b7a2550c6f730db92079624e287
jaman7 <krzysztofbalon123@gmail.com>
2025-06-18 03:52:27 +0200

    add detailPage and style

--- src/shared/components/LazyImage/LazyImage.tsx
+++ src/shared/components/LazyImage/LazyImage.tsx
@@ -7,12 +7,11 @@ export interface ILazyImage {
   src?: string | null;
   alt?: string;
   onClick?: () => void;
-  placeholderSrc?: string;
 }
 
 const DEFAULT_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
 
-const LazyImage: React.FC<ILazyImage> = ({ id, className, src = null, alt, onClick, placeholderSrc = '' }) => {
+const LazyImage: React.FC<ILazyImage> = ({ id, className, src = null, alt, onClick }) => {
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(false);
   const imgRef = useRef<HTMLImageElement>(null);
